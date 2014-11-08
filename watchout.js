f00d99a9ac1ff9da31307f6d320253f173263907
lizportnoy <lizport10@gmail.com>
2014-11-07 19:38:19 -0800

    finished basic

--- watchout.js
+++ watchout.js
@@ -6,11 +6,6 @@ var gameOptions = {
   collisions: [0]
 };
 
-var playerStorage = [{
-    x: [476],
-    y: [300]
-  }];
-
 var gameDisplay = d3.select('.scoreboard')
   .selectAll('span')
   .data([gameOptions.highScore, gameOptions.currentScore, gameOptions.collisions]);
@@ -21,6 +16,63 @@ var incrementScore = function(){
   gameDisplay.text(function(d){return d[0]});
 }
 
+var gameOver = function() {
+  gameOptions.highScore[0] = Math.max(gameOptions.currentScore[0], gameOptions.highScore[0]);
+  gameOptions.currentScore[0] = 0;
+  gameOptions.collisions[0]++;
+}
+
+var playerStorage = {
+    x: [476],
+    y: [300]
+  };
+
+var movePlayer = function(d) {
+  detectCollision();
+  d3.select(this).attr("transform", function(d){
+      return "translate(" + [ d.x[0],d.y[0] ] + ")"
+  });
+  d.x[0] = Math.min(Math.max(d.x[0] + d3.event.dx, 5), 950);
+  d.y[0] = Math.min(Math.max(d.y[0] + d3.event.dy, 5), 595);
+};
+
+
+var drag = d3.behavior.drag()
+  .origin(function(d) { return d; })
+  .on('drag', movePlayer);
+
+var player = d3.select('.gameBoard')
+  .selectAll('.player')
+  .data([playerStorage])
+  .enter()
+  .append('circle')
+  .attr('class', 'player')
+  .attr("transform", "translate(" + 476 + "," + 300 + ")")
+  // .attr('x', function(d){return d.x[0]})
+  // .attr('y', function(d){return d.y[0]})
+  // .attr('width', 30)
+  // .attr('height', 30)
+  .attr('r',20)
+  .attr('fill','pink')
+  .call(drag)
+
+
+var detectCollision = function () {
+  // if the x,y coordinates of the player match any of the x,y coordinates of the enemies within a set distance
+  var _distance = function(player, enemy) {
+    return Math.sqrt(Math.pow(player.x[0]-enemy.cx,2)+Math.pow(player.y[0]-enemy.cy,2));
+  };
+  for(var i = 0; i<enemies.length; i++){
+    if(_distance(playerStorage, enemies[i])<30){
+      gameOver();
+    }
+  }
+};
+  //.on("dragstart", function () {console.log('dragged')})
+  //.on("dragend"/*, dragended*/);
+
+var enemies = [];
+
 var randWidth = function() {
     return Math.random()*930+10;
 };
@@ -29,24 +81,6 @@ var randHeight = function() {
     return Math.random()*580+10;
 };
 
-var gameOver = function() {
-  gameOptions.highScore[0] = Math.max(gameOptions.currentScore[0], gameOptions.highScore[0]);
-  gameOptions.currentScore[0] = 0;
-  gameOptions.collisions[0]++;
-}
-
-var moveEnemies = function () {
-  incrementScore();
-  circles.transition()
-  .duration(500)
-  .attr('cx', randWidth)
-  .attr('cy', randHeight)
-  .attr('r', 10)
-  .attr('fill', 'white')
-}
-
-var enemies = [];
-
 var Enemy = function(){
   this.cx = randWidth();
   this.cy = randHeight();
@@ -54,49 +88,48 @@ var Enemy = function(){
   this.fill = 'white';
 }
 
+Enemy.prototype.update = function() {
+  this.cx = randWidth();
+  this.cy = randHeight();
+};
+
 for(var i = 0; i< 15; i++){
   enemies.push(new Enemy());
 }
+var moveEnemies = function () {
+  incrementScore();
+  //detectCollision();
 
-var movePlayer = function(d) {
-  d3.select(this).attr("transform", function(d){
-      return "translate(" + [ d.x[0],d.y[0] ] + ")"
-  });
-  d.x[0] += d3.event.dx;
-  d.y[0] += d3.event.dy;
-};
+  circles.transition()
+    .duration(500)
+    .tween('custom', detectCollision)
+    .attr('cx', function(d) {d.update(); return d.cx})
+    .attr('cy', function(d) { return d.cy})
+    .attr('r', function(d) {return d.r})
+    .attr('fill', function(d) {return d.fill})
+}
+  // .attr('cx', randWidth)
+  // .attr('cy', randHeight)
+  // .attr('r', 10)
+  // .attr('fill', 'white')
+
 
 var circles = d3.select('.gameBoard')
-  .selectAll('circle')
+  .selectAll('.enemy')
   .data(enemies)
   .enter()
   .append('circle')
+  .attr('class','enemy')
+  // on appending, enact into motion the set interval
   .attr('cx', function(d) { return d.cx})
   .attr('cy', function(d) { return d.cy})
   .attr('r', function(d) {return d.r})
   .attr('fill', function(d) {return d.fill})
   //.on('mouseover',gameOver);
 
-var drag = d3.behavior.drag()
-  .origin(function(d) { return d; })
-  .on('drag', movePlayer);
-  //.on("dragstart", function () {console.log('dragged')})
-  //.on("dragend"/*, dragended*/);
 
-var player = d3.select('.gameBoard')
-  .selectAll('rect')
-  .data(playerStorage)
-  .enter()
-  .append('rect')
-  .attr("transform", "translate(" + 476 + "," + 300 + ")")
-  // .attr('x', function(d){return d.x[0]})
-  // .attr('y', function(d){return d.y[0]})
-  .attr('width', 30)
-  .attr('height', 30)
-  .attr('fill','pink')
-  .call(drag)
 
-window.setInterval(moveEnemies, 1000);
+window.setInterval(moveEnemies, 2000);
 
 
 
